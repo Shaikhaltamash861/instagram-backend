7f64df7a251ed926ed383da90f19edc4ae1e9f68
Shaikhaltamash861 <altamash2c2@gmail.com>
2023-07-27 12:50:13 +0530

    uploads addeds

--- router/Router.js
+++ router/Router.js
@@ -1,7 +1,7 @@
 const express=require('express')
 
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/',
+const upload = multer({
 limits: {  fileSize: 1048576, fieldSize:  25 * 1024 * 1024 } })
 
 const router=express.Router();
